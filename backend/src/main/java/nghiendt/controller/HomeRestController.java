c5a5abb14fd528bfc797a4c6b8002b8f9cc824e6
PhamNhatKhanhs <KhanhPN.B23DCC088@stu.ptit.edu.vn>
2026-04-01 23:43:45 +0700

    feat(backend): add home, banner, and setting controllers

---
+++ backend/src/main/java/nghiendt/controller/HomeRestController.java
@@ -0,0 +1,97 @@
+package nghiendt.controller;
+
+import lombok.SneakyThrows;
+import nghiendt.dto.UserDTO;
+import nghiendt.entity.User;
+import nghiendt.exception.ResourceNotFoundException;
+import nghiendt.payload.JwtResponse;
+import nghiendt.payload.JwtTokenUtil;
+import nghiendt.service.AuthenticationService;
+import nghiendt.service.MailerService;
+import nghiendt.service.impl.UserDetailsImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
+
+@CrossOrigin("*")
+@RestController
+public class HomeRestController {
+
+    @Autowired
+    private AuthenticationManager authManager;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private UserDetailsImpl userDetailsService;
+
+    @Autowired
+    MailerService mailerService;
+    @Autowired
+    AuthenticationService authService;
+
+    @RequestMapping({"/", "/index", "home"})
+    public ModelAndView index() {
+        ModelAndView view = new ModelAndView();
+        view.setViewName("index");
+        return view;
+    }
+
+    private void authenticate(String username, String password) throws Exception {
+        try {
+            authManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (DisabledException e) {
+            throw new Exception("USER_DISABLED", e);
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
+    }
+
+    @PostMapping("auth/login")
+    public ResponseEntity<?> login(@RequestBody UserDTO userDTO) throws Exception {
+        authenticate(userDTO.getUsername(), userDTO.getPassword());
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(userDTO.getUsername());
+        final String token = jwtTokenUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
+
+    @PostMapping("auth/register")
+    public ResponseEntity<?> register(@RequestBody UserDTO user) throws Exception {
+        return ResponseEntity.ok(userDetailsService.save(user));
+    }
+
+    @PostMapping("auth/forgot-password")
+    public ResponseEntity<Void> sendMailToken(@RequestBody String email) {
+        if (authService.sendResetMail(email)) {
+            return ResponseEntity.ok().build();
+        }
+        throw new ResourceNotFoundException("User not exists with email: " + email);
+    }
+
+    @SneakyThrows
+    @GetMapping("auth/reset-password")
+    public ResponseEntity<User> checkToken(@RequestParam Optional<String> token) {
+        ResourceNotFoundException exception = new ResourceNotFoundException("Cannot find token: " + token);
+        String tokenVal = token.orElseThrow(() -> exception);
+        User user = authService.findByToken(tokenVal);
+        if (user != null) {
+            return ResponseEntity.ok(user);
+        }
+        throw exception;
+    }
+
+    @PostMapping("auth/change-password")
+    public ResponseEntity<Void> changePassword(@RequestBody User user) throws Exception {
+        authenticate(user.getUsername(), user.getPassword());
+        return authService.changePassword(user) ? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
+    }
+}
---
+++ backend/src/main/java/nghiendt/controller/SettingRestController.java
@@ -0,0 +1,56 @@
+package nghiendt.controller;
+
+import nghiendt.entity.Setting;
+import nghiendt.exception.ResourceNotFoundException;
+import nghiendt.repository.SettingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping(value = "/api/setting")
+public class SettingRestController {
+    @Autowired
+    private SettingRepository settingRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Setting>> getAllSettings() {
+        List<Setting> listContact = settingRepository.findAll();
+        if (listContact.isEmpty()) {
+            return new ResponseEntity<List<Setting>>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<List<Setting>>(listContact, HttpStatus.OK);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Setting> getSettingsById(@PathVariable int id) {
+        Setting setting = settingRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Contact not exist with id: " + id));
+        return ResponseEntity.ok(setting);
+    }
+
+    @PostMapping
+    public Setting createSettings(@RequestBody Setting setting) {
+        return settingRepository.save(setting);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Setting> updateSettings(@PathVariable("id") int id, @RequestBody Setting setting) {
+        Setting updateSetting = settingRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Contact not exist with id: " + id));
+        settingRepository.save(setting);
+        return ResponseEntity.ok(updateSetting);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<HttpStatus> deleteSettings(@PathVariable int id) {
+        Setting setting = settingRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Contact not exist with id: " + id));
+        settingRepository.delete(setting);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
